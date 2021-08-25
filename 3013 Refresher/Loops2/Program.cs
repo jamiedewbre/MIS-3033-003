bd63409d340edba00a837317820e5a76d98bfcfa
Jamie Dewbre <jamiedewbre@ou.edu>
2021-08-25 16:04:20 -0500

    Completed loop 2 problem.

---
+++ 3013 Refresher/Loops2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Loops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            int examCount = 0;
+
+            do
+            {
+                examCount++;
+                Console.WriteLine($"Please enter your score for Exam {examCount.ToString("N0")} *e.g. 90% as .9*");
+                sum = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("DO you have another score to enter? Yes or no");
+
+            } while (Console.ReadLine().ToLower() == "yes");
+            // console.readline().tolower()[0] == 'y');
+
+            Console.WriteLine($"Your average exam grade is {(sum/examCount).ToString("P")}");
+        }
+    }
+}
