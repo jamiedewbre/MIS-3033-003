c675c3541b82f50e41c30053c7e96e9c4501a280
Jamie Dewbre <jamiedewbre@ou.edu>
2021-08-30 15:52:58 -0500

    Finished arrays 1 review.

---
+++ 3013 Refresher/Arrays1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Arrays1
+{
+    class Program
+    {
+        //need to know size of arrays
+        static void Main(string[] args)
+        {
+            string[] fruits = new string[5];
+
+
+            fruits[0] = "Apples";
+            fruits[1] = "Oranges";
+            fruits[2] = "Bananas";
+            fruits[3] = "Grapes";
+            fruits[4] = "Blueberries";
+
+            double[] prices = { 0.99, 0.50, 0.50, 2.99, 1.99 };
+
+            Console.WriteLine("What fruit would you like the price for? >>");
+            string fruit = Console.ReadLine();
+
+            int fruitIndex = -1;
+
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                if (fruits[i] == fruit)
+                {
+                    fruitIndex = i;
+                    break;
+                }
+
+            }
+
+            if (fruitIndex >= 0)
+            {
+            Console.WriteLine($"The price of fruit is {prices[fruitIndex].ToString("C2")}");
+                
+            }
+
+            else
+            {
+                Console.WriteLine($"We do not carry {fruit} here. Go elsewhere.");
+            }
+            
+      
+        }
+    }
+}
