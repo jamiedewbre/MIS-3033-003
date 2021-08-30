533cf8a63fb4dc6e77f0ff64b590bc5cb5afe75a
Jamie Dewbre <jamiedewbre@ou.edu>
2021-08-30 15:59:39 -0500

    Completed simplified version of arrays 2.

---
+++ 3013 Refresher/Arrays2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] lowercaseLetters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            string firstName = "" + lowercaseLetters[9] + lowercaseLetters[0] + lowercaseLetters[12] + lowercaseLetters[8] + lowercaseLetters[4];
+            // firstName = lowercaseLetters[9].ToString() + ...
+
+            Console.WriteLine(firstName);
+        }
+    }
+}
